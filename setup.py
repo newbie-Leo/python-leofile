b92ffa6b3859301684e9275093fa6c73f4836d97
newbie-Leo <lrz4119@163.com>
2016-07-21 13:09:07 +0800

    create setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name="leofile",
+    version="0.10",
+    description="",
+    author="newbie-Leo",
+    url="https://github.com/newbie-Leo/python-leofile",
+)
