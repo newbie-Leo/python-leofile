2cd8a3cc961ce3e64b7ddee013183f3c08ee69bd
newbie-Leo <lrz4119@163.com>
2016-07-21 14:18:47 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
     name="leofile",
     version="0.10",
     description="",
     author="newbie-Leo",
     url="https://github.com/newbie-Leo/python-leofile",
+    py_modules=["leofile", ],
+    packages=find_packages(),
 )
