336396f7db7f329a4147ff3a689e47132e7c8ff1
newbie-Leo <lrz4119@163.com>
2016-07-21 11:32:58 +0800

    0721

--- leofile.py
+++ leofile.py
@@ -2,11 +2,21 @@ import os
 
 
 class Leofile(object):
+    r"""class Leofile make files as object ^_^.
+    usage:
+        path = '/etc'
+        etcObj = Leofile(path)
+        nginxConfObj = etcObj.nginx['nginx.conf'].open('r')
+        nginxConfObj.read()
+    """
 
     def __init__(self, path, **kwargs):
         self.path = path
 
     def list(self):
+        '''
+        return listdir
+        '''
         return os.listdir(self.path)
 
     def __getattr__(self, attr):
@@ -16,4 +26,23 @@ class Leofile(object):
         return self.__getattr__(attr)
 
     def open(self, mode):
+        '''
+        return opened file
+        '''
         return open(self.path, mode)
+
+    @property
+    def isdir(self):
+        return os.path.isdir(self.path)
+
+    @property
+    def isfile(self):
+        return os.path.isfile(self.path)
+
+    @property
+    def parrent(self):
+        return Leofile(os.path.join(self.path, '..'))
+
+    @property
+    def stat(self):
+        return os.stat(self.path)
