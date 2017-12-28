2d01a5a4c02bd3be07f1ba97d925bf163b9916fe
liurz <liurunze@starnet.com.cn>
2017-12-28 10:23:15 +0800

    leofile

--- leofile.py
+++ leofile.py
@@ -11,10 +11,10 @@ class Leofile(object):
     """
 
     def __init__(self, path, **kwargs):
-        self.path = path
+        self._path = path
 
     def __str__(self):
-        return "Leofile of %s" % self.path
+        return "Leofile of %s" % self._path
 
     def __iter__(self):
         return iter(self.list())
@@ -22,36 +22,36 @@ class Leofile(object):
     def __repr__(self):
         return "<%s>" % self.__str__()
 
+    def __getattr__(self, attr):
+        return Leofile(os.path.join(self._path, attr))
+
+    def __getitem__(self, attr):
+        return self.__getattr__(attr)
+
+    @property
+    def isdir(self):
+        return os.path.isdir(self._path)
+
+    @property
+    def isfile(self):
+        return os.path.isfile(self._path)
+
+    @property
+    def parrent(self):
+        return Leofile(os.path.join(self._path, os.path.pardir))
+
+    @property
+    def stat(self):
+        return os.stat(self._path)
+
     def list(self):
         '''
         return listdir
         '''
-        return [Leofile(os.path.abspath(i)) for i in os.listdir(self.path)]
-
-    def __getattr__(self, attr):
-        return Leofile(os.path.join(self.path, attr))
-
-    def __getitem__(self, attr):
-        return self.__getattr__(attr)
+        return [Leofile(os.path.join(self._path, i)) for i in os.listdir(self._path)]
 
     def open(self, mode):
         '''
         return opened file
         '''
-        return open(self.path, mode)
-
-    @property
-    def isdir(self):
-        return os.path.isdir(self.path)
-
-    @property
-    def isfile(self):
-        return os.path.isfile(self.path)
-
-    @property
-    def parrent(self):
-        return Leofile(os.path.abspath(os.path.join(self.path, '..')))
-
-    @property
-    def stat(self):
-        return os.stat(self.path)
+        return open(self._path, mode)
